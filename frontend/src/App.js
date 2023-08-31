0e503eb8cc0335deb5d8410bc7ba1268f831e529
Megan <megantech@hotmail.com>
2023-08-31 14:21:58 +0000

    Initial Navbar added using bootstrap with logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
-import Button from "react-bootstrap/Button";
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   );
 }
