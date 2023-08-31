452b9588586434f92c3baca4b59dd2f818914756
Megan <megantech@hotmail.com>
2023-08-31 14:37:06 +0000

    add initial styling to Navbar and more spacing and sizing added to App module css

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="48" /> HotFeet
@@ -13,13 +14,13 @@ const NavBar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <Nav.Link>
-              <i className="fas fa-house"></i> Home
+              <i className="fas fa-home"></i> Home
             </Nav.Link>
             <Nav.Link>
               <i className="fas fa-sign-in-alt"></i> Sign in
             </Nav.Link>
             <Nav.Link>
-              <i className="fa-solid fa-user-plus"></i> Sign up
+              <i className="fas fa-user-plus"></i> Sign up
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
