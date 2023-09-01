3009f1c10f947b4cbb4ce8239d7ba9af1a7c45bf
Megan <megantech@hotmail.com>
2023-09-01 14:06:34 +0000

    Re-saved requirement.txt

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -8,7 +8,6 @@
 
  axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
  axios.defaults.withCredentials = true;
-
-
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
+ 
+ export const axiosReq = axios.create();
+ export const axiosRes = axios.create();
