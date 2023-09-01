49399dac44c8f323d6dbcbc5ab5ab19b60ea5e8b
Megan <megantech@hotmail.com>
2023-09-01 19:09:51 +0000

    Removed print stamtement from database in settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -164,7 +164,6 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-    print('connected and saying hello')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
