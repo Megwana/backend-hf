b7633ba3b5e7575e0099c04b5f2308d5682efd93
Megan <megantech@hotmail.com>
2023-09-01 19:05:05 +0000

    Add api/ to the beggining of API URLs excluding home page path and admin. Add 404 handler in urls.

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -6,18 +6,20 @@ from .views import logout_route
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api/api-auth/', include('rest_framework.urls')),
     # Logout route has to be above the default one to be matched first
-    path('dj-rest-auth/logout/', logout_route),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/dj-rest-auth/logout/', logout_route),
+    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
-        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
+        'api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
-    path('', include('profiles.urls')),
-    path('', include('posts.urls')),
-    path('', include('comments.urls')),
-    path('', include('likes.urls')),
-    path('', include('dislikes.urls')),
-    path('', include('followers.urls')),
-    path('', include('bookmarks.urls')),
+    path('api/', include('profiles.urls')),
+    path('api/', include('posts.urls')),
+    path('api/', include('comments.urls')),
+    path('api/', include('likes.urls')),
+    path('api/', include('dislikes.urls')),
+    path('api/', include('followers.urls')),
+    path('api/', include('bookmarks.urls')),
 ]
+
+handler404 = TemplateView.as_view(template_name='index.html')
